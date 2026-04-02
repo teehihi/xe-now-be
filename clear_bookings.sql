20b3743621939e92b7c81db6d8ab08e355e4ba3d
teehihi <tee21052005@gmail.com>
2026-04-03 01:46:58 +0700

    fix render.yaml

--- clear_bookings.sql
+++
@@ -1,7 +0,0 @@
--- Xóa tất cả các bản ghi có khóa ngoại tới Booking trước
-DELETE FROM Payment;
-DELETE FROM Contract;
-DELETE FROM Invoice;
-
--- Cuối cùng xóa các bản ghi trong bảng Booking
-DELETE FROM Booking;
